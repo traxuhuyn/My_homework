3403c5ddd4bf1823beb59661e41d926e9b00c5a0
traxuhuyn <62512985+traxuhuyn@users.noreply.github.com>
2020-03-22 23:16:18 +0700

    Add files via upload

---
+++ Firsthomework.js
@@ -0,0 +1,14 @@
+// // Write a program that calculates the area of a circle. The circle radius is entered by user
+const first = prompt("Bán kính đường tròn");
+const second = Number(first) ** 2;
+const S = Number(second) * 3.14;
+const name = "Diện tích đường tròn =";
+const totalString = `${name}. ${S}`;
+console.log(totalString);
+
+
+// // Write a program that converts Celsius (0C) into Fahrenheit (0F)
+// const C = prompt("Nhập nhiệt độ cần đổi");
+// const CtoF = (C * 9 / 5 + 32);
+// const totalString = `${C}. ${CtoF}`;
+// console.log(totalString);
